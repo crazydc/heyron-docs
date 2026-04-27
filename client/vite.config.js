afe430e56548ecd95682b8a186bd8dd1deb25a76
Dale <d_c_1990@hotmail.com>
2026-04-27 21:43:14 +0000

    Fix: Add React plugin for Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 5173,
     proxy: {
@@ -10,4 +12,4 @@ export default defineConfig({
       }
     }
   }
-});
+})
